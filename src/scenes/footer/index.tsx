1fa5484da42916dd0b186cdc348b09af125b4e62
isurubandara1 <isurumbandara@gmail.com>
2024-07-21 11:34:44 +0530

    footer crete

--- src/scenes/footer/index.tsx
+++ src/scenes/footer/index.tsx
@@ -3,26 +3,37 @@ import Logo from "@/assets/Logo.png";
 const Footer = () => {
   return (
     <footer className="bg-primary-100 py-16">
-      <div className="justify-content mx-auto w-5/6 gap-16 md:flex">
-        <div className="mt-16 basis-1/2 md:mt-0">
+      <div className="mx-auto w-5/6 md:flex justify-between gap-16">
+        {/* Logo and Description */}
+        <div className="mt-16 md:mt-0 md:basis-1/2">
           <img alt="logo" src={Logo} />
           <p className="my-5">
-            Lorem vitae ut augue auctor faucibus eget eget ut libero. Elementum
-            purus et arcu massa dictum condimentum. Augue scelerisque iaculis
-            orci ut habitant laoreet. Iaculis tristique.
+            We are dedicated to providing you with exceptional service and
+            quality. Our goal is to enhance your lifestyle through our
+            comprehensive offerings. From fitness to wellness, we have something
+            for everyone.
           </p>
-          <p>© Samsor All Rights Reserved.</p>
+          <p>© Isuru. All Rights Reserved.</p>
         </div>
-        <div className="mt-16 basis-1/4 md:mt-0">
+        
+        {/* Links Section */}
+        <div className="mt-16 md:mt-0 md:basis-1/4">
           <h4 className="font-bold">Links</h4>
-          <p className="my-5">Massa orci senectus</p>
-          <p className="my-5">Et gravida id et etiam</p>
-          <p>Ullamcorper vivamus</p>
+          <p className="my-5">Discover our various services</p>
+          <p className="my-5">Learn more about our programs</p>
+          <p>Contact our support team</p>
         </div>
-        <div className="mt-16 basis-1/4 md:mt-0">
+        
+        {/* Contact Us Section */}
+        <div className="mt-16 md:mt-0 md:basis-1/4">
           <h4 className="font-bold">Contact Us</h4>
-          <p className="my-5">Tempus metus mattis risus volutpat egestas.</p>
-          <p>(333)425-6825</p>
+          <p className="my-5">
+            For inquiries and support, feel free to reach out to us. We're here
+            to help you achieve your goals and answer any questions you may
+            have.
+          </p>
+          <p>070 456 3212</p>
+          <p>037 457 4242</p>
         </div>
       </div>
     </footer>
